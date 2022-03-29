5e016692bad8fb1e0e459cf59979aff1420a82f5
Mubashir78 <mubashirrindbaloch@gmail.com>
2022-03-29 17:23:18 +0500

    G_T_N_G v1.0.1
    . Decreased time required to print text serially, as well as decreased time between prints to show text quickly
    . Made some optimizations to the script code
    . Added an .exe version so that Python and VSC are not required to run the program

--- Guess_The_Num_Game.py
+++ Guess_The_Num_Game.py
@@ -1,6 +1,9 @@
-from time import sleep
+# Programmed by: Mubashir Ahmed OR known as Mubashir78 on GitHub
+# https://www.github.com/Mubashir78
+
 from random import randint
 from sys import stdout
+from time import sleep
 
 class color:
     PURPLE = '\033[95m'
@@ -22,70 +25,81 @@ def print_slow(str):
     for letter in str:
         stdout.write(letter)
         stdout.flush()
-        sleep(0.031)
+        sleep(0.025)
     print(" ")
 
 def startup():
     print_slow(color.BOLD+"=======================\n Guess The Number Game \n======================="+color.END)
-    sleep(1.6)
-    intro()
+    sleep(1)
+    return intro()
 
 def intro():
     print_slow(color.BOLD+f"\nIn this game, you have to guess a number from 1 to 10!"+color.END)
-    sleep(2)
-    main()
+    sleep(1.3)
+    return main(max_num)
 
-def set_max_num():
+def set_max_num(max_num):
     print_slow(color.ITALIC+"==========\n Settings \n=========="+color.END)
-    sleep(0.8)
+    sleep(0.5)
+
     print_slow(color.BOLD+"\nHere you can set the max number to guess upto.")
-    sleep(1.2)
+    sleep(0.8)
+
     print_slow("The default is 10.")
-    sleep(1)
+    sleep(0.6)
+
     while True:
-        global max_num
         max_num = input(color.BOLD+color.CYAN+"\nSet the max number to: "+color.END)
         try:
             max_num = int(max_num)
             print_slow(color.BOLD+color.GREEN+f"Max number changed to {max_num} successfully."+color.END)
-            sleep(1.2)
+            sleep(0.8)
+
             print_slow(color.ITALIC+"Starting the game with new settings... "+color.END)
-            sleep(1.2)
+            sleep(0.6)
+
             print_slow(color.BOLD+f"\nNow you have to guess a number from 1 to {max_num}!"+color.END)
-            sleep(1.8)
+            sleep(1)
+
             return max_num
+
         except ValueError:
              print_slow("%s" % error_code)
-             sleep(1)    
+             sleep(0.5)
 
 def exit_code():
     print_slow(color.BOLD+"\nThe script will now exit.")
-    sleep(1)
+    sleep(0.5)
+
     print_slow("Thanks for playing!"+color.END)
     sleep(0.8)
-    exit()          
+
+    return
                                     
-def main():
-    global max_num
+def main(max_num):
     ran_num = randint(1,max_num)
+
     while True:
         num = input(color.BOLD+color.CYAN+"\nEnter the number: "+color.END).lower()
         
         if num == "show" or num == "reveal":
             print_slow(color.BOLD+f"The number was {ran_num}."+color.END)
-            sleep(1.2)
-            sub_main()
+            sleep(0.6)
+
+            return retry_code()
             
         elif num =="set":
-            max_num = set_max_num()
-            main()
+            max_num = set_max_num(max_num)
+
+            return main(max_num)
             
         elif num == "exit" or num == "quit":
-            exit_code()
+            return exit_code()
             
         else:
              try:
                 num = int(num)
+
              except ValueError:
                 print_slow("%s" % error_code)
                 sleep(1)
@@ -94,7 +108,8 @@ def main():
              if num == ran_num:
                 print_slow(color.BOLD+color.GREEN+f"You guessed correct! It was {ran_num}!"+color.END)
                 sleep(1.3)
-                sub_main()
+
+                return retry_code()
             
              elif num < 1:
                 print_slow(color.BOLD+color.RED+f"That's too low!\nPlease make sure to enter from 1 to {max_num}."+color.END)
@@ -112,20 +127,25 @@ def main():
                 print_slow("Guess higher!")
                 sleep(1)   
 
-def sub_main():
+def retry_code():
     choice = input(color.BOLD+color.BLUE+"\nDo you wish to play again?(y/n): "+color.END).lower()
     
     if choice == "y":
         print_slow(color.ITALIC+"Restarting..."+color.END)
         sleep(1)
-        main() 
+
+        return main(max_num) 
         
     elif choice == "n":
-        exit_code()
+        return exit_code()
         
     else:
         print_slow("%s" % error_code)
         sleep(1.3)
-        sub_main()
+
+        return retry_code()
 
 startup()
+
+# Programmed by: Mubashir Ahmed OR known as Mubashir78 on GitHub
+# https://www.github.com/Mubashir78
